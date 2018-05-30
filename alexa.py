6eb768baae7b9afbb58e78dacbc08730cc4e450e
notherno <kitanow.takaaki@gmail.com>
2018-05-31 00:08:34 +0900

    Add meshi feature

--- alexa.py
+++ alexa.py
@@ -1,4 +1,5 @@
 import json
+import random
 from flask import Flask
 from flask_ask import Ask, statement
 
@@ -8,9 +9,11 @@ ask = Ask(app, '/')
 host = '0.0.0.0'
 PORT = 5000
 
+meshi = ['すき家', '松屋', '吉野家']
+
 @ask.intent('Syukkin')
 def syukkin(firstname):
-    speech_text = "ステータスを出勤にしました"
+    speech_text = 'それはそうと今日の昼はどこにしますか？おすすめは{0}です'.format(random.choice(meshi))
     return statement(speech_text).simple_card('Syukkin', speech_text)
 
 if __name__ == '__main__':
