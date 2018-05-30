b02996b2ec37ed644e62927d938a731c39a16e6b
notherno <kitanow.takaaki@gmail.com>
2018-05-30 23:23:23 +0900

    Use Dockerfile

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: pipenv run python alexa.py
--- alexa.py
+++ alexa.py
@@ -5,10 +5,12 @@ from flask_ask import Ask, statement
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+PORT = 5000
+
 @ask.intent('Syukkin')
 def syukkin(firstname):
     speech_text = "ステータスを出勤にしました"
     return statement(speech_text).simple_card('Syukkin', speech_text)
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=PORT)
